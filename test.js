db67608206a3786458ccb9d6cb0f453bba2be9dd
ty028473 <fgh1887763145@gmail.com>
2021-10-16 13:49:52 +0800

    promise

---
+++ test.js
@@ -0,0 +1,14 @@
+const fs =require("fs");
+
+new Promise((resolve,reject)=>{
+    fs.readFile("input.txt","utf-8",(err,data)=>{
+        if(err){
+            reject;
+            console.log(err)
+
+        }else{
+            resolve(data);
+            console.log(data)
+        }
+        });
+});
